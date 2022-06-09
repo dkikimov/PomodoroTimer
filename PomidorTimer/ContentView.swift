67227ac28179c96e26b3a2fef0d0bb0fee498307
Daniil Kikimov <katsushooter@gmail.com>
2022-06-09 15:21:16 +0600

    Initial Commit

---
+++ PomidorTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomidorTimer
+//
+//  Created by Даник 💪 on 09.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomidorTimer/PomidorTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomidorTimerApp.swift
+//  PomidorTimer
+//
+//  Created by Даник 💪 on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PomidorTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
